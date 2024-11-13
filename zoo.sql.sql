4053f45185f52fb64f3869a22aa47193ca8c1029
Marko Radoš <rados.marko05@gmail.com>
2024-11-13 17:42:14 +0100

    Završeno kreiranje prema era dijagramu

---
+++ zoo.sql.sql
@@ -0,0 +1,14 @@
+--create databse zoo;
+
+--drop database zoo;
+
+--use database zoo;
+
+create table ziovtinja(
+sifra int,
+vrsta varchar(50),
+ime varchar(50),
+djelatnik int,
+prostorija int,
+datum int
+);
