153fcd9bd58ffa5da6bc4cf28bde72d229951632
Marko Radoš <rados.marko05@gmail.com>
2024-11-13 18:22:47 +0100

    zoo tablica

--- zoo.sql
+++ zoo.sql
@@ -1,25 +1,41 @@
 use master;
 go
 
-drop database if exists edunovawp6;
+drop database if exists zoo;
 go
 
-create database edunovawp6;
+create database zoo;
 go
 
-use edunovawp6;
+use zoo;
 go
-
-create table zivotinja(
-sifra int not null primary key identity(1,1),
-vrsta varchar(50) not null
-ime varchar(50) not null,
-djelatnik int,
-prostorija int,
-datum int
-);
 
 create table prostorija(
 sifra int not null primary key identity(1,1),
+dimenzije varchar(30) not null,
+maksbroj int not null,
+mjesto varchar(30),
+);
 
+create table djelatnik(
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+iban varchar(50)
+);
+
+create table datum(
+sifra int not null primary key identity(1,1),
+datumrodenja datetime not null,
+datumdolaska datetime not null,
+datumsmrti datetime not null
+);
+
+create table zivotinja(
+sifra int not null primary key identity(1,1),
+vrsta varchar(50) not null,
+ime varchar(50) not null,
+djelatnik int not null references djelatnik (sifra),
+prostorija int not null references prostorija (sifra),
+datum int not null references datum(sifra)
 );
