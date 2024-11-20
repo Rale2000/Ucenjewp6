ebf41088a9500f9e1c82cd7c95c3e5cf760c9bb1
Rale2000 <rados.marko05@gmail.com>
2024-11-20 17:52:25 +0100

    Primjeri promjene i brisanje podataka iz tablica

---
+++ update_delete.sql
@@ -0,0 +1,37 @@
+﻿-- UPDATE naredba
+
+select * from smjerovi;
+update smjerovi set cijena = cijena * 0.9;
+select * from smjerovi;
+
+-- uvećajte sve cijene za 35%
+select * from smjerovi;
+update smjerovi set cijena = cijena * 1.35;
+select * from smjerovi;
+
+-- smanjite svim smjerovima cijenu za 10 eura
+select * from smjerovi; 
+update smjerovi set cijena = cijena - 10;
+select * from smjerovi;
+
+
+-- uvijet vanjskog ključa
+update grupe set smjer=8 where sifra=1;
+
+
+-- DELETE naredba
+
+select * from smjerovi;
+delete from smjerovi where sifra>8;
+select * from smjerovi;
+
+select * from grupe;
+update grupe set smjer=1 where sifra=1;
+
+delete grupe where sifra=1;
+
+select * from clanovi;
+
+update clanovi set grupa=2 where grupa=1;
+
+
